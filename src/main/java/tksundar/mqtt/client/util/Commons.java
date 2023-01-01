104df1085fea4ebdbae92282ebac0b273fd6eea5
tksundar <53440744+tksundar@users.noreply.github.com>
2023-01-01 10:54:05 +0530

    Refactored to read log file location in a platform independent way

--- src/main/java/tksundar/mqtt/client/util/Commons.java
+++ src/main/java/tksundar/mqtt/client/util/Commons.java
@@ -1,5 +1,6 @@
 package tksundar.mqtt.client.util;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.PropertyResourceBundle;
@@ -17,9 +18,11 @@ public class Commons {
     private static final String LOG_FILE_KEY = "logFileLocation";
     private static final ResourceBundle bundle = getBundle();
 
+    private static final String fileSeparator = File.separator;
+
     private static ResourceBundle getBundle() {
         ResourceBundle bundle;
-        try{
+        try {
             bundle = new PropertyResourceBundle(new FileReader("app.properties"));
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -28,7 +31,7 @@ public class Commons {
         return bundle;
     }
 
-    public enum LoggerType {CONSOLE, FILE, STREAM, MEMORY}
+    public enum LoggerType {CONSOLE, FILE}
 
     public static Logger getLogger(String loggerName, LoggerType... types) {
         Logger logger = Logger.getLogger(loggerName);
@@ -42,16 +45,30 @@ public class Commons {
                 }
                 case FILE -> {
                     COMMONS_LOGGER.fine("setting file logger");
-                    Handler handler ;
+                    Handler handler;
                     try {
                         String homeDir = System.getenv("HOME");
                         String logFileLocation = bundle.getString(LOG_FILE_KEY);
-                        handler = new FileHandler(homeDir+"/"+logFileLocation+"/mqtt.log");
+                        File f = new File(homeDir + fileSeparator + logFileLocation);
+                        if (!f.exists()) {
+                            boolean created = f.mkdir();
+                            if (created) {
+                                COMMONS_LOGGER.fine("log dir created");
+                            } else {
+                                COMMONS_LOGGER.severe("Error creating file");
+                                COMMONS_LOGGER.throwing(Commons.class.getName(), "getLogger",
+                                        new RuntimeException("Could not create dir"));
+                            }
+                        }
+                        System.out.println(homeDir + fileSeparator + logFileLocation + fileSeparator + "mqtt.log");
+                        handler = new FileHandler(homeDir + fileSeparator + logFileLocation + fileSeparator + "mqtt.log");
+                        handler.setFormatter(new SimpleFormatter());
+                        logger.addHandler(handler);
+
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
-                    handler.setFormatter(new SimpleFormatter());
-                    logger.addHandler(handler);
+
                 }
                 default -> COMMONS_LOGGER.warning("Unexpected value: " + type);
             }
