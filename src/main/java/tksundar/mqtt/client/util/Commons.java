b6f3a0bdb80e18b7ee3f133e4329d9cacf1eb686
tksundar <53440744+tksundar@users.noreply.github.com>
2023-01-01 10:15:46 +0530

    Refactored to read log file location in a platform independent way

--- src/main/java/tksundar/mqtt/client/util/Commons.java
+++ src/main/java/tksundar/mqtt/client/util/Commons.java
@@ -1,6 +1,9 @@
 package tksundar.mqtt.client.util;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
 import java.util.logging.*;
 
 /**
@@ -11,6 +14,19 @@ import java.util.logging.*;
 public class Commons {
 
     private static final Logger COMMONS_LOGGER = Logger.getLogger(Commons.class.getName());
+    private static final String LOG_FILE_KEY = "logFileLocation";
+    private static final ResourceBundle bundle = getBundle();
+
+    private static ResourceBundle getBundle() {
+        ResourceBundle bundle;
+        try{
+            bundle = new PropertyResourceBundle(new FileReader("app.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return bundle;
+    }
 
     public enum LoggerType {CONSOLE, FILE, STREAM, MEMORY}
 
@@ -28,7 +44,9 @@ public class Commons {
                     COMMONS_LOGGER.fine("setting file logger");
                     Handler handler ;
                     try {
-                        handler = new FileHandler("mqtt.log");
+                        String homeDir = System.getenv("HOME");
+                        String logFileLocation = bundle.getString(LOG_FILE_KEY);
+                        handler = new FileHandler(homeDir+"/"+logFileLocation+"/mqtt.log");
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
